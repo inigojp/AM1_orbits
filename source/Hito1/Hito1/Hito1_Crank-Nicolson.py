9ce63bdd884cc1e11092721c0be9d2450dc4ac88
inigojp <ij.palacios.2018@alumnos.urjc.es>
2023-09-26 19:45:14 +0200

    L

--- source/Hito1/Hito1/Hito1_Crank-Nicolson.py
+++ source/Hito1/Hito1/Hito1_Crank-Nicolson.py
@@ -1,83 +1,38 @@
-from numpy import array, zeros
+from numpy import array, zeros, linspace
 import matplotlib.pyplot as plt
+from scipy.optimize import fsolve
+
+N = 10000                       # PASOS DE INTEGRACION
+dt = 0.01                        # INTERVALO ENTRE PASOS
+t_values = array(zeros(N))
 
 def F_Kepler(U):
-    
-    x, y, vx, vy = [U[0], U[1], U[2], U[3]]
+ 
+    x, y, vx, vy = [U[0,:], U[1,:], U[2,:], U[3,:]]
     mr = (x**2 + y**2) ** 1.5
     
     return array( [vx, vy, -x/mr, -y/mr] )
 
-def F_Kepler_2():
-N = 10000
-U = array( [1, 0, 0, 1] )
-dt = 0.001
-x = array( zeros(N) )
-y = array( zeros(N) )
-vx = array( zeros(N) )
-vy = array( zeros(N) )
+U = array(zeros((4,N)))
 
-x[0] = U[0]
-y[0] = U[1]
-vx[0] = U[2]
-vy[0] = U[3]
+U[0,0] = 1
+U[1,0] = 0
+U[2,0] = 0
+U[3,0] = 1
 
-for i in range(0, N):
-    F = F_Kepler( U )
-    U = U + dt/2 * ( F )
-    x[i] = U[0]
-    y[i] = U[1]
-    
-plt.plot(x, y)
+
+def CN( dt, U, N ):
+
+    for i in range(N-1):
+        
+        def CN_res(x):
+            
+            return x - U_temp - dt/2 * F_Kepler(x)
+        U_temp = U[:,i] + dt/2 * F_Kepler(U[:,i])
+        
+        U[:,i+1] = fsolve(CN_res,U[:,i])
+        
+    return U
+U = CN(dt, U, N)
+plt.plot(U[0,N-1],U[1,N-1])
 plt.show()
-
-import numpy
-import matplotlib.pyplot as plt
-
-# Discretización en el tiempo
-num_steps = 1000
-dt = 0.01
-times = np.linspace(0, T, num_steps)
-
-# Inicialización de arrays para almacenar los resultados
-x = zeros(num_steps)
-y = zeros(num_steps)
-vx = zeros(num_steps)
-vy = zeros(num_steps)
-
-# Condiciones iniciales
-x[0] = 1
-y[0] = 0.0
-vx[0] = 0.0
-vy[0] = 1
-
-# Método de Crank-Nicolson para la integración numérica
-for i in range(1, num_steps):
-    r = sqrt(x[i-1]**2 + y[i-1]**2)
-    ax_n = - (G * M * x[i-1]) / (r**3)
-    ay_n = - (G * M * y[i-1]) / (r**3)
-
-    # Aproximación de la derivada temporal usando Crank-Nicolson
-    ax_n1 = - (G * M * x[i]) / (sqrt(x[i]**2 + y[i]**2)**3)
-    ay_n1 = - (G * M * y[i]) / (sqrt(x[i]**2 + y[i]**2)**3)
-
-    # Actualización de posiciones y velocidades usando Crank-Nicolson
-    x[i] = x[i-1] + dt * 0.5 * (vx[i-1] + vx[i] + dt * 0.5 * (ax_n + ax_n1))
-    y[i] = y[i-1] + dt * 0.5 * (vy[i-1] + vy[i] + dt * 0.5 * (ay_n + ay_n1))
-    vx[i] = vx[i-1] + dt * 0.5 * (ax_n + ax_n1)
-    vy[i] = vy[i-1] + dt * 0.5 * (ay_n + ay_n1)
-
-# Graficar la órbita
-plt.figure(figsize=(8, 6))
-plt.plot(x, y)
-plt.xlabel('Coordenada x')
-plt.ylabel('Coordenada y')
-plt.title('Órbita Kepleriana en 2D (método de Crank-Nicolson)')
-plt.axis('equal')
-plt.grid(True)
-plt.show()
-
-
-
-
-
