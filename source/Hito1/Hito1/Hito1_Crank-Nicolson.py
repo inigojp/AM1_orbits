ce6d16e234f97197b199fd89d44a7bb9c62748f7
inigojp <ij.palacios.2018@alumnos.urjc.es>
2023-09-27 21:08:23 +0200

    hito1_f

--- source/Hito1/Hito1/Hito1_Crank-Nicolson.py
+++ source/Hito1/Hito1/Hito1_Crank-Nicolson.py
@@ -8,18 +8,20 @@ t_values = array(zeros(N))
 
 def F_Kepler(U):
  
-    x, y, vx, vy = [U[0,:], U[1,:], U[2,:], U[3,:]]
+    x, y, vx, vy = [U[0], U[1], U[2], U[3]]
     mr = (x**2 + y**2) ** 1.5
     
     return array( [vx, vy, -x/mr, -y/mr] )
 
 U = array(zeros((4,N)))
-
+x = array( zeros(N) )
+y = array( zeros(N) )
 U[0,0] = 1
 U[1,0] = 0
 U[2,0] = 0
 U[3,0] = 1
-
+x[0] = U[0,0]
+y[0] = U[1,0]
 
 def CN( dt, U, N ):
 
@@ -31,8 +33,9 @@ def CN( dt, U, N ):
         U_temp = U[:,i] + dt/2 * F_Kepler(U[:,i])
         
         U[:,i+1] = fsolve(CN_res,U[:,i])
-        
+        x[i+1] = U[0,i+1]
+        y[i+1] = U[1,i+1]
     return U
 U = CN(dt, U, N)
-plt.plot(U[0,N-1],U[1,N-1])
+plt.plot(x,y)
 plt.show()
