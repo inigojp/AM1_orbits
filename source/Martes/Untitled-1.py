8eb7b1eafa7b83499188bba500195fba3ba33f91
inigojp <ij.palacios.2018@alumnos.urjc.es>
2023-11-19 12:48:46 +0100

    organizacion

--- source/Martes/Untitled-1.py
+++ source/Martes/Untitled-1.py
@@ -1,5 +1,6 @@
 
 from numpy import array, zeros, linspace, arange
+from Schemes import Euler, RK4, Crank_Nicolson, Inverse_Euler
 import matplotlib.pyplot as plt
 from scipy.optimize import fsolve, newton
 import math
@@ -46,36 +47,9 @@ def ODE_Rayleigh2(U, t):
     vy=x-x**3-y+3*math.cos(t)
 
     return array( [vx, vy] )
+
 ## Esquemas numéricos
-def Euler(F,U,t0, tf):
-    dt = tf-t0
-    return U + dt * F(U,t0)
-    
-def Crank_Nicolson(F,i):
-    dt = tf-t0
-    def g(x):
-        return x - a -dt/2 * (F(a,t0) + F(x,t0))
-    a = U[:,i]
-    
-    return newton(g, U)
 
-def RK4(F,U,t0, tf ):
-    dt = tf-t0
-    k1 = F( U, t0 )
-    k2 = F( U +k1*dt/2, t0 + dt/2 )
-    k3 = F( U +k2*dt/2, t0 + dt/2 )
-    k4 = F( U + k3*dt, t0 + dt )
-     
-    k = 1/6 * (k1 + 2*k2 + 2*k3 + k4)
-    
-    return U + dt * k
-
-def Inverse_Euler(F,i):
-    def g(x):
-        return x - a - dt * F(x,t) 
-    a = U[:,i]
-    
-    return newton(g, U[:,i])
 
 ##Integrar 
 def Integrate_ODE(U, F, t, scheme):
@@ -99,15 +73,15 @@ U2[:,0] = array( [1, 0.01] )
 #U = array(zeros((1,N)))
 #U[:,0] = 1.1
 
-U = Integrate_ODE(U, ODE_Rayleigh, t, RK4)
-U2 = Integrate_ODE(U2, ODE_Rayleigh2, t, RK4)
+#U = Integrate_ODE(U, ODE_Rayleigh, t, RK4)
+U2 = Integrate_ODE(U2, ODE_Rayleigh2, t, Inverse_Euler)
 
 
 f1 = plt.figure()
 #plt.plot(U[0,5000:N],U[1,5000:N])
 plt.plot(U2[0,10000:N],U2[1,10000:N])
-f2 = plt.figure()
-plt.plot(U2[0,0000:N])
+#f2 = plt.figure()
+#plt.plot(U2[0,0000:N])
 #plt.plot(U2[1,5000:N])
 
 #plt.plot(U2[0,:])
