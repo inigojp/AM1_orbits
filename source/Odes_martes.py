639ba7a161e2c30923a64d5282b247cfc5c777cc
inigojp <ij.palacios.2018@alumnos.urjc.es>
2023-11-25 15:21:30 +0100

    o

--- source/Odes_martes.py
+++ source/Odes_martes.py
@@ -22,18 +22,16 @@ t = linspace(0,N*d_t, N+1)
 
 U0 = array(zeros((3,len(t)-1)))
 U0[:,0] = array( [0.5, 1, 2] )
-U1 = array(zeros((3,len(t)-1)))
-U1[:,0] = array( [0.5, 1, 2] )
 
-U = Integrate_ODE(U0, Rossler, t, RK4)
-U2 = Integrate_ODE(U1, Rossler2, t, RK4)
+
+U = Integrate_ODE(U0, Lorentz, t, RK4)
+
 
 f1 = plt.figure()
 ax = plt.axes(projection ='3d')
 ax.plot3D(U[0,:],U[1,:],U[2,:])
 f2 = plt.figure()
-ax = plt.axes(projection ='3d')
-ax.plot3D(U2[0,:],U2[1,:],U2[2,:])
+
 #plt.plot(U2[1,5000:N])
 
 
